34764a1676fb25ba82fffdca6c20de6b167aa89a
Niklas Korz <niklas@niklaskorz.de>
2021-11-23 10:31:28 +0100

    Use ResizeObserver for detecting changes in content height

--- src/ViewZone.svelte
+++ src/ViewZone.svelte
@@ -1,54 +1,60 @@
 <script lang="ts">
-  import type { editor } from "monaco-editor";
-  import { onDestroy, onMount } from "svelte";
-  import portal from "./portal";
+    import type { editor } from "monaco-editor";
+    import { onDestroy, onMount } from "svelte";
+    import portal from "./portal";
 
-  const domNode = document.createElement("div");
-  let heightInPx = 0;
-  let zoneId: string | null = null;
+    const domNode = document.createElement("div");
+    domNode.style.zIndex = "100";
+    let container: HTMLElement;
+    let heightInPx = 0;
+    let zoneId: string | null = null;
 
-  export let editor: editor.ICodeEditor;
-  export let line: number;
-  export let column: number;
+    export let editor: editor.ICodeEditor;
+    export let line: number;
+    export let column: number;
 
-  onMount(() => {
-    document.body.appendChild(domNode);
-    heightInPx = domNode.clientHeight;
-  });
-
-  onDestroy(() => {
-    editor.changeViewZones((accessor) => {
-      if (zoneId) {
-        accessor.removeZone(zoneId);
-      }
+    const resizeObserver = new ResizeObserver(() => {
+        heightInPx = container.offsetHeight;
     });
-  });
 
-  $: if (heightInPx) {
-    editor.changeViewZones((accessor) => {
-      if (zoneId) {
-        accessor.removeZone(zoneId);
-      }
-      zoneId = accessor.addZone({
-        afterLineNumber: line,
-        domNode,
-        heightInPx,
-      });
+    onMount(() => {
+        document.body.appendChild(domNode);
+        resizeObserver.observe(container);
+        console.log("mounted");
     });
-  }
+
+    onDestroy(() => {
+        resizeObserver.disconnect();
+        editor.changeViewZones((accessor) => {
+            if (zoneId) {
+                accessor.removeZone(zoneId);
+            }
+        });
+    });
+
+    $: if (heightInPx) {
+        resizeObserver.disconnect();
+        editor.changeViewZones((accessor) => {
+            if (zoneId) {
+                accessor.removeZone(zoneId);
+            }
+            zoneId = accessor.addZone({
+                afterLineNumber: line,
+                domNode,
+                heightInPx,
+            });
+        });
+        requestAnimationFrame(() => {
+            resizeObserver.observe(container);
+        });
+    }
 </script>
 
 <div
-  class="view-zone"
-  style="padding-left: {editor.getOffsetForColumn(line, column)}px;"
-  use:portal={domNode}
-  bind:clientHeight={heightInPx}
+    class="view-zone"
+    style="padding-left: {editor.getOffsetForColumn(line, column)}px;"
+    use:portal={domNode}
+    bind:this={container}
 >
-  <slot />
+    <slot />
 </div>
-
-<style>
-  .view-zone {
-    z-index: 100;
-  }
-</style>
