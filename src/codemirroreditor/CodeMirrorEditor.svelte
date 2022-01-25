195b9e9afd85eaec66187af9eafa6db318d01c2f
Niklas Korz <niklas@niklaskorz.de>
2022-01-25 18:33:55 +0100

    Experiment with inline projections in CodeMirror

--- src/codemirroreditor/CodeMirrorEditor.svelte
+++ src/codemirroreditor/CodeMirrorEditor.svelte
@@ -1,89 +1,49 @@
 <script lang="ts">
-    import { EditorState, basicSetup } from "@codemirror/basic-setup";
-    import {
-        EditorView,
-        keymap,
-        Decoration,
-        WidgetType,
-        DecorationSet,
-        ViewPlugin,
-        PluginValue,
-    } from "@codemirror/view";
-    import { indentWithTab } from "@codemirror/commands";
-    import { autocompletion } from "@codemirror/autocomplete";
-    import { javascript } from "@codemirror/lang-javascript";
-    import { onMount } from "svelte";
-    import { example } from "../code";
+  import { EditorState, basicSetup } from "@codemirror/basic-setup";
+  import { EditorView, keymap } from "@codemirror/view";
+  import { indentWithTab } from "@codemirror/commands";
+  import { autocompletion } from "@codemirror/autocomplete";
+  import { javascript } from "@codemirror/lang-javascript";
+  import { onDestroy, onMount } from "svelte";
+  import { example } from "../code";
+  import { checkboxPlugin } from "./CheckboxPlugin";
+  import { textPlugin } from "./TextPlugin";
 
-    let container: HTMLDivElement;
-    let editor: EditorView;
+  let container: HTMLDivElement;
+  let editor: EditorView;
 
-    class CheckboxWidget extends WidgetType {
-        constructor(readonly checked: boolean) {
-            super();
-        }
-
-        eq(other: CheckboxWidget): boolean {
-            return other.checked == this.checked;
-        }
-
-        toDOM(): HTMLElement {
-            let wrap = document.createElement("span");
-            wrap.setAttribute("aria-hidden", "true");
-            wrap.className = "cm-boolean-toggle";
-            let box = wrap.appendChild(document.createElement("input"));
-            box.type = "checkbox";
-            box.checked = this.checked;
-            return wrap;
-        }
-
-        ignoreEvent(): boolean {
-            return false;
-        }
-    }
-
-    class CheckboxPlugin implements PluginValue {
-        decorations: DecorationSet;
-
-        constructor(view: EditorView) {
-            let replacement = Decoration.replace({
-                widget: new CheckboxWidget(true),
-            });
-            this.decorations = Decoration.set([replacement.range(50, 70)]);
-        }
-
-        update() {}
-
-        destroy() {}
-    }
-
-    const checkboxPlugin = ViewPlugin.fromClass(CheckboxPlugin, {
-        decorations: (v) => v.decorations,
+  onMount(() => {
+    editor = new EditorView({
+      state: EditorState.create({
+        doc: example,
+        extensions: [
+          basicSetup,
+          keymap.of([indentWithTab]),
+          autocompletion(),
+          javascript({ typescript: true }),
+          checkboxPlugin,
+          textPlugin,
+        ],
+      }),
+      parent: container,
     });
+  });
 
-    onMount(() => {
-        editor = new EditorView({
-            state: EditorState.create({
-                doc: example,
-                extensions: [
-                    basicSetup,
-                    keymap.of([indentWithTab]),
-                    autocompletion(),
-                    javascript({ typescript: true }),
-                    checkboxPlugin,
-                ],
-            }),
-            parent: container,
-        });
-    });
+  onDestroy(() => {
+    editor.destroy();
+  });
 </script>
 
 <div bind:this={container} />
 
 <style>
-    :global(.cm-content) {
-        font-family: "JetBrains Mono", "SF Mono", "Menlo", "Consolas", "Monaco",
-            "Courier New", monospace;
-        font-size: 16px;
-    }
+  :global(.cm-content) {
+    font-family: "JetBrains Mono", "SF Mono", "Menlo", "Consolas", "Monaco",
+      "Courier New", monospace;
+    font-size: 16px;
+  }
+
+  :global(.cm-text-widget) {
+    caret-color: black;
+  }
 </style>
---
+++ src/codemirroreditor/TextPlugin.ts
@@ -0,0 +1,109 @@
+import { syntaxTree } from "@codemirror/language";
+import {
+  Decoration,
+  DecorationSet,
+  EditorView,
+  PluginValue,
+  ViewPlugin,
+  ViewUpdate,
+  WidgetType,
+  Range,
+} from "@codemirror/view";
+
+class TextWidget extends WidgetType {
+  constructor(
+    readonly value: string,
+    readonly from: number,
+    readonly to: number
+  ) {
+    super();
+  }
+
+  eq(other: TextWidget): boolean {
+    return other.value === this.value;
+  }
+
+  toDOM(): HTMLElement {
+    let wrap = document.createElement("span");
+    wrap.className = "cm-text-widget";
+    let box = wrap.appendChild(document.createElement("input"));
+    box.type = "text";
+    box.value = this.value.slice(1, this.value.length - 1);
+    box.dataset.from = this.from.toString();
+    box.dataset.to = this.to.toString();
+    return wrap;
+  }
+
+  ignoreEvent(event: Event): boolean {
+    return event.type !== "input";
+  }
+}
+
+function texts(view: EditorView) {
+  let widgets: Array<Range<Decoration>> = [];
+  for (let { from, to } of view.visibleRanges) {
+    syntaxTree(view.state).iterate({
+      from,
+      to,
+      enter(type, from, to) {
+        if (type.name == "String") {
+          let value = view.state.doc.sliceString(from, to);
+          let deco = Decoration.replace({
+            widget: new TextWidget(value, from, to),
+          });
+          widgets.push(deco.range(from, to));
+        }
+      },
+    });
+  }
+  return Decoration.set(widgets);
+}
+
+function updateText(view: EditorView, value: string, from: number, to: number) {
+  console.log(value);
+  view.dispatch({
+    changes: {
+      from,
+      to,
+      insert: JSON.stringify(value),
+    },
+  });
+  return true;
+}
+
+class TextPlugin implements PluginValue {
+  decorations: DecorationSet;
+
+  constructor(view: EditorView) {
+    this.decorations = texts(view);
+  }
+
+  update(update: ViewUpdate) {
+    if (update.docChanged || update.viewportChanged) {
+      console.log(update.view.state.doc);
+      this.decorations = texts(update.view);
+    }
+  }
+
+  destroy() {}
+}
+
+export const textPlugin = ViewPlugin.fromClass(TextPlugin, {
+  decorations: (v) => v.decorations,
+  eventHandlers: {
+    input(e, view) {
+      let target = e.target as HTMLInputElement;
+      if (
+        target.nodeName == "INPUT" &&
+        target.parentElement?.classList.contains("cm-text-widget")
+      ) {
+        return updateText(
+          view,
+          target.value,
+          parseInt(target.dataset.from!),
+          parseInt(target.dataset.to!)
+        );
+      }
+    },
+  },
+});
