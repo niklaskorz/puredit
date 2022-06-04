8dd003b4ab2db3ecaa097f38f9df1eaba92ce41a
Niklas Korz <niklas@niklaskorz.de>
2022-06-04 14:48:38 +0200

    Experimenting with different projection definitions

---
+++ apps/generate/templates.ts
@@ -0,0 +1,5 @@
+const definitionTemplate = () => `
+import { arg, contextVariable } "@puredit/parser";
+import type { Projection } from "@puredit/projections/types";
+import { tsParser } from "./parser";
+`;
