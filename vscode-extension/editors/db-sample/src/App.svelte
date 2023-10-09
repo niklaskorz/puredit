a6ab9572c9471c1535c81a9d36d76fcecb6f6ec8
Andre Trump <anditru@web.de>
2023-10-09 12:56:15 +0200

    Simplify src structure

--- vscode-extension/editors/db-sample/src/App.svelte
+++ vscode-extension/editors/db-sample/src/App.svelte
@@ -1,28 +1,94 @@
 <script lang="ts">
-  import TypescriptEditor from "./ts/Editor.svelte";
+  import DarkMode from "svelte-dark-mode";
+  import type { Theme } from "svelte-dark-mode/types/DarkMode.svelte";
+  import { basicSetup } from "codemirror";
+  import { EditorState, Annotation, Compartment } from "@codemirror/state";
+  import type { Extension } from "@codemirror/state";
+  import { EditorView, keymap } from "@codemirror/view";
+  import { indentWithTab } from "@codemirror/commands";
+  import { autocompletion } from "@codemirror/autocomplete";
+  import { onDestroy, onMount } from "svelte";
+  import { projectionPlugin, completions } from "@puredit/projections";
+  import { oneDark } from "@codemirror/theme-one-dark";
+  import {
+    typescript,
+    completionSource as typescriptCompletionSource,
+  } from "@puredit/codemirror-typescript";
+  import { indentationMarkers } from "@replit/codemirror-indentation-markers";
+  import { projectionPluginConfig } from "./projections";
+
+  let theme: Theme | undefined;
+  let container: HTMLDivElement;
+  let projectionalEditor: EditorView | undefined;
+
+  const syncChangeAnnotation = Annotation.define<boolean>();
+  const darkThemeCompartment = new Compartment();
+
+  onMount(() => {
+    const extensions: Extension[] = [
+      basicSetup,
+      keymap.of([indentWithTab]),
+      darkThemeCompartment.of(theme === "dark" ? oneDark : []),
+      indentationMarkers(),
+      EditorView.theme({
+        ".cm-scroller": {
+          fontFamily: "var(--mono-font, monospace)",
+          fontSize: "14px",
+        },
+        ".cm-tooltip": {
+          fontFamily: "var(--system-font, sans-serif)",
+        },
+      }),
+    ];
+    const projectionalEditorExtensions = extensions.concat([
+      typescript({ disableCompletions: true, disableTooltips: true }),
+      projectionPlugin(projectionPluginConfig),
+      autocompletion({
+        activateOnTyping: true,
+        override: [completions, typescriptCompletionSource],
+      }),
+    ]);
+
+    projectionalEditor = new EditorView({
+      state: EditorState.create({
+        extensions: projectionalEditorExtensions,
+      }),
+      parent: container,
+      dispatch(tr) {
+        projectionalEditor!.update([tr]);
+        if (!tr.changes.empty && !tr.annotation(syncChangeAnnotation)) {
+          // TODO: Propagte changes to VS Code
+        }
+      },
+    });
+
+    projectionalEditor.dispatch();
+  });
+
+  onDestroy(() => {
+    projectionalEditor?.destroy();
+  });
+
+  function onThemeChange(theme?: Theme) {
+    const transaction = {
+      effects: [
+        darkThemeCompartment.reconfigure(theme === "dark" ? oneDark : []),
+      ],
+    };
+    projectionalEditor?.dispatch(transaction);
+  }
+
+  // Dynamically update color scheme on theme change
+  $: onThemeChange(theme);
 </script>
 
-<main>
-  <TypescriptEditor />
-</main>
+<DarkMode bind:theme />
+
+<div class="container" bind:this={container} />
 
 <style>
-  :global(body) {
-    box-sizing: border-box;
-  }
-
-  :global(*, *::before, *::after) {
-    box-sizing: inherit;
-  }
-
-  main {
-    width: 100vw;
-    height: 100vh;
-    overflow: auto;
-    --system-font: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
-      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
-      sans-serif;
-    --mono-font: "JetBrains Mono", "SF Mono", "Menlo", "Consolas", "Monaco",
-      "Courier New", monospace;
+  .container {
+    width: 100%;
+    height: 100%;
   }
 </style>
